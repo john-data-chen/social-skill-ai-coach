bde57ae483892f3bbc18d8936ef11f984bc6ab66
JianAn Chen <john.data.chen@gmail.com>
2026-06-20 16:23:07 +0800

    feat: setup shadcn/ui

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import "./globals.css"
+
+import type { Metadata } from "next"
+import { Noto_Sans_TC, Geist } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
+
+const notoSansTC = Noto_Sans_TC({
+  subsets: ["latin"],
+  variable: "--font-noto-sans-tc"
+})
+
+export const metadata: Metadata = {
+  title: "Social Skills AI Coach",
+  description: "AI Social Skills Coach"
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" className={cn("font-sans", geist.variable)}>
+      <body className={`${notoSansTC.variable} antialiased`}>{children}</body>
+    </html>
+  )
+}
